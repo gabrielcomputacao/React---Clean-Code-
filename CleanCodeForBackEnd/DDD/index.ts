ff2f7239a4e7cc4adccac2ab8ce4f615231f7415
gabriel <gabrielcomputacao@outlook.com>
2024-03-24 06:57:56 -0300

    feat: DDD principles

---
+++ CleanCodeForBackEnd/DDD/index.ts
@@ -0,0 +1,31 @@
+// DDD = desgin dirigido a dominio
+
+/* 
+    Prática de pensar como vai fazer o seu software
+
+    Desenho da aplicação, momento de escopo do projeto
+
+    Dominio é o problema que voce está resolvendo e seu contexto
+
+    Exemplo: 
+        E-commerce
+
+            O ambiente e os problemas são chamados de dominio, não é algo que vai virar algo dentro do codigo
+            Dominio é tudo que entra nos problemas a serem resolvidos com relação ao ramo ou atividade do software
+
+            Olha para dentro do projeto e dividi em subdominios
+
+            Subdominio do e=commerce
+            -> DIferentes areas com pequenos entendimentos menores sobre o projeto maior, Exemplo:
+                - Logística
+                - Faturamento
+                - Pagamento
+                - Estoque
+
+    Domain Experts
+        Pessoa que sabe tudo sobre o dominio
+
+    Entidades - Estruturas ou atores dentro de cada subdominio
+    Casos de uso - ações dentro do subdominio
+
+*/
